aba28bea3a7c7b851915dc05f9dffb9566287868
Tobin Bradley <tobin.bradley@gmail.com>
2023-10-09 14:34:50 -0400

    set grid margin for finger space

--- src/lib/AssetList.svelte
+++ src/lib/AssetList.svelte
@@ -66,7 +66,7 @@
   </div>
   {/if}
 
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-10 gap-y-14 ">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-10 gap-y-14 mx-4 ">
 
     {#each filter(data, filterString) as el}
     <Card record={el} />
